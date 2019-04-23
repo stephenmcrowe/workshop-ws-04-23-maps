073f5b5d3945ee97546f05fe242ca1cefc46eced
Stephen Crowe <crowe.stephen.m@gmail.com>
2019-04-23 15:13:28 -0400

    all the workshop

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.919ab4c5ea505e267c0fa336a2e0d7b4.js"
 );
 
 workbox.clientsClaim();
